bab808d8798d0788d922ab934e958312819ce7bb
Jhonny515 <jonatascemporcentojesus@gmail.com>
2023-01-24 15:38:59 -0300

    arquivo html e js para manipulação de data

---
+++ aula6.js
@@ -0,0 +1,19 @@
+// new Date()
+// new Date(valor)
+// new Date(dataString)
+// new Date(ano, mes, dia, hora, minuto, segundo, milissegundo)
+
+// const data = new Date(2001, 12, 11)
+// data.getDay()
+// data.getFullYear()
+// data.toISOString()
+
+// // Métodos de classe
+// Date.now()
+// Date.UTC()
+
+
+// const data = dayjs()
+// console.log(data.format())
+// console.log(data.format('YYYY MM DD'))
+// console.log(data.format('DD MM YYYY'))
